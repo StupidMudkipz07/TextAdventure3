7c2afd398ad0586b2746991bee28da4b719a7cde
StupidMudkipz07 <lukas@schnell-johansson.se>
2026-09-09 00:36:24 +0200

    Dags att jobba på en story imorgon!!!2

--- Program.cs
+++ Program.cs
@@ -2,14 +2,8 @@
 
 Console.Clear();
 
-ItemManager itemManager = new();
-
-FightManager fightManager = new(itemManager);
-
 //världens mest läsbara kod här!!!!!!
-LocationManager locationManager = new(new() { Hp = 100, Damage = 67, Defense = 1, StealChance = 67 },
-fightManager, 
- "Torget");
+LocationManager locationManager = new(new() { Hp = 100, Damage = 67, Defense = 1, StealChance = 67 }, new(new()), "Torget");
 
 locationManager.PlayRealGAMEOMMG();
 
