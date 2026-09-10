f89596013d19069ec1c1b59e068777a5ba8faace
StupidMudkipz07 <lukas@schnell-johansson.se>
2026-09-10 20:59:28 +0200

    ok slopiog

--- Item.cs
+++ Item.cs
@@ -2,6 +2,8 @@ struct Item
 {
     [JsonInclude] public string Name;
     [JsonInclude] public string Description;
+    [JsonInclude] public int DefenseBuff;
+    [JsonInclude] public int DamageBuff;
 
     public void PrintItem()
     {
